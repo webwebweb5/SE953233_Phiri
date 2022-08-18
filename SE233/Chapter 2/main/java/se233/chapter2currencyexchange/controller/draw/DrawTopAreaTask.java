7c25f511e2bf1264c26f6e4e8e6fcf8068c3a31b
w4yZ <68845415+webwebweb5@users.noreply.github.com>
2022-08-18 10:07:16 +0700

    Exercise 2

--- SE233/Chapter 2/main/java/se233/chapter2currencyexchange/controller/draw/DrawTopAreaTask.java
+++ SE233/Chapter 2/main/java/se233/chapter2currencyexchange/controller/draw/DrawTopAreaTask.java
@@ -1,4 +1,34 @@
 package se233.chapter2currencyexchange.controller.draw;
 
-public class DrawTopAreaTask {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import se233.chapter2currencyexchange.model.Currency;
+import se233.chapter2currencyexchange.view.CurrencyPane;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+public class DrawTopAreaTask implements Callable<HBox> {
+    Currency currency;
+    CurrencyPane currencyPane;
+    Button watch;
+    Button unwatch;
+    Button delete;
+    public DrawTopAreaTask(Currency currency, Button watch, Button unwatch, Button delete){
+        this.currency = currency;
+        this.watch = watch;
+        this.unwatch = unwatch;
+        this.delete = delete;
+    }
+    @Override
+    public HBox call() throws ExecutionException, InterruptedException {
+        HBox topArea = new HBox(10);
+        topArea.setPadding(new Insets(5));
+        topArea.getChildren().addAll(watch, unwatch, delete);
+        ((HBox) topArea).setAlignment(Pos.CENTER_RIGHT);
+        return topArea;
+    }
+    // set button
 }
